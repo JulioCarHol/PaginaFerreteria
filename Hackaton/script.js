adc435acf813cf269f33e0aa6d2f31b76b8f9495
DianaOstios <dmarcela.ostios@gmail.com>
2023-06-23 13:56:23 -0500

    Se agrega formulario

---
+++ Hackaton/script.js
@@ -0,0 +1,7 @@
+
+let productos = ["BombaElectrica", "Compresor", "Cortafrio","Destornilladores","Kit Tornilleria", "Taladros", "TomaCorriente"];
+let precios = ["$400.000","$2.000.000","$3.000.000","$200.000","$300.000","$250.000","$30.000"];
+let stock = ["4","2","3","2","3","2","3"];
+function mostrarprecio(){
+  productos.get
+};
