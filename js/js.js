13e1989f919e8d6121e87ac51c88e80b50645911
JulioCarHol <juliocardenas_003@hotmail.com>
2023-06-24 22:56:37 -0500

    Se modifican cosas del codigo para mejorarlo

--- js/js.js
+++ js/js.js
@@ -18,8 +18,6 @@ function addToCart(elem) {
         if(elem.className == "price"){
             getprice = elem.innerText.trim();
         }
-        
-        // Se agregó una validación para verificar la clase del elemento.
         if (elem.classList.contains("productname")) {
             getproductName = elem.innerText.trim();
         }
@@ -31,23 +29,19 @@ function addToCart(elem) {
       productname : getproductName,
       price : getprice
      };
-     
-     // Se eliminó esta línea, ya que no era necesaria.
-     // También se cambió el nombre de la variable para evitar confusiones con otros nombres similares.
      let stringProductObjetoJSON= JSON.stringify(product);
       
      if(!sessionStorage.getItem('cart')){
-         cart.push(stringProductObjetoJSON);//Se agrega objeto a array y luego se convierte en un JSON String. 
-         stringCart= JSON.stringify(cart);//Se guarda el Array como un objeto JSON String en sessionStorage
-         addedToCart(getproductName);//Mensaje de éxito al agregar producto al carrito
-         updateCartTotal();//Actualiza los valores totales del carrito
-         
+         cart.push(stringProductObjetoJSON);
+         stringCart= JSON.stringify(cart);
+         addedToCart(getproductName);
+         updateCartTotal();
      } else{
           cart= JSON.parse(sessionStorage.getItem('cart'));
-          cart.push(stringProductObjetoJSON);//Se agrega objeto a array y luego se convierte en un JSON String. 
-          stringCart= JSON.stringify(cart);//Se guarda el Array como un objeto JSON String en sessionStorage
-          addedToCart(getproductName);//Mensaje de éxito al agregar producto al carrito
-          updateCartTotal();//Actualiza los valores totales del carrito   
+          cart.push(stringProductObjetoJSON);
+          stringCart= JSON.stringify(cart);
+          addedToCart(getproductName);
+          updateCartTotal(); 
      }
      
      sessionStorage.setItem('cart', stringCart); //Guardando variable 'stringCarro' en sessionStorage.
@@ -66,24 +60,21 @@ function updateCartTotal(){
 
         for (let i in cart){
             let x=JSON.parse(cart[i]);
-            price=parseFloat(x.price.split('$')[1]);//Extrae el valor numérico del precio a partir de la cadena que incluye $.
-            productname=x.productname;//Asigna nombre de producto.
-
-            // Se agregó una fila por cada elemento encontrado en el carrito.
-            carttable += '<tr><td>' + productname + '</td><td>$' + price + '</td></tr>';
-            
+            price=parseFloat(x.price.split('$')[1]);
+            productname=x.productname;
+            carttable += '<tr><td>' + productname + '</td><td>$' + price + '</td></tr>';          
             total+=price;  
         }        
    }
    
    document.getElementById("total").innerHTML="$"+total;
    document.getElementById("itemsquantity").innerHTML=items;
-   document.getElementById("carttable").innerHTML = carttable;//se agrega todo lo almacenado en la variable 'carritotabla'
+   document.getElementById("carttable").innerHTML = carttable;
 }
 
 function addedToCart(pname) {
   let message=pname+" ha sido agregado.";
-  alerts.innerHTML=message;//en lugar de crear una nueva variable se utiliza directamente la creada previamente sin definir: "alerts"
+  alerts.innerHTML=message;
   
   if(!alerts.classList.contains("message")){
       alerts.classList.add("message");
